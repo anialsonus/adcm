f23196506ea89d0c2bc16bdc4441ab85b38501c3
dbardin90 <d.bardin@arenadata.io>
2022-12-23 14:47:05 +0300

    ADCM-3467 - fixes and changes for config/group config

--- web/src/app/shared/configuration/main/config.component.ts
+++ web/src/app/shared/configuration/main/config.component.ts
@@ -82,7 +82,10 @@ export class ConfigComponent extends SocketListenerDirective implements OnChange
 
     service.worker$.subscribe((data) => {
       this.getConfigUrlFromWorker();
-      this._getConfig(data.current.config).subscribe();
+      if (data.current.config && !this.isLoading) {
+        this.service.changeService(data.current.typeName);
+        this._getConfig(data.current.config).subscribe();
+      }
     });
   }
 
@@ -227,7 +230,11 @@ export class ConfigComponent extends SocketListenerDirective implements OnChange
   private _getConfig(url: string): Observable<IConfig> {
     this.isLoading = true;
     return this.service.getConfig(url).pipe(
-      tap((config) => this.attributeUniqId = this.attributesSrv.init(config.attr)),
+      tap((config) => {
+        if (!this.attributeUniqId) {
+          this.attributeUniqId = this.attributesSrv.init(config.attr);
+        }
+      }),
       tap((c) => this.rawConfig.next(c)),
       finalize(() => this.isLoading = false),
       catchError(() => {
